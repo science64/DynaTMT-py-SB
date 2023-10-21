bbc1a032cc37f27a99a3cc23b485f60071713795
Süleyman Bozkurt <sbozkurt.mbg@gmail.com>
2023-10-22 01:32:54 +0200

    version 2.5

--- DynaTMT_SB/__init__.py
+++ DynaTMT_SB/__init__.py
@@ -4,4 +4,5 @@ __version__ = '2.5'
 __updated_date__ = '2023-10-22'
 __description__ = 'DynaTMT - updates and fixes.'
 __license__ = 'MIT'
-import DynaTMT
+from DynaTMT import PD_input
+from DynaTMT import plain_text_input
