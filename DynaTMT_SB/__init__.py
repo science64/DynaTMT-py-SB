9062d143aea6ed88dd1f6abaab69bc6dd26f8e3a
Süleyman Bozkurt <sbozkurt.mbg@gmail.com>
2023-10-22 01:30:34 +0200

    version 2.5

--- DynaTMT_SB/__init__.py
+++ DynaTMT_SB/__init__.py
@@ -1,6 +1,7 @@
-# __author__ = 'Kevin Klann'
-# __updated_by__ = 'Süleyman Bozkurt'
-# __version__ = '2.5'
-# __updated_date__ = '2023-10-22'
-# __description__ = 'DynaTMT - updates and fixes.'
-# __license__ = 'MIT'
+__author__ = 'Kevin Klann'
+__updated_by__ = 'Süleyman Bozkurt'
+__version__ = '2.5'
+__updated_date__ = '2023-10-22'
+__description__ = 'DynaTMT - updates and fixes.'
+__license__ = 'MIT'
+import DynaTMT
